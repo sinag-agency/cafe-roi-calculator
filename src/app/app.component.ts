38ff927fbe326587540c59c05dc7b8aea529e8f9
Nicu Listana <Nicu Listana>
2025-01-01 22:57:32 -0800

    stash

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,72 @@
-import { Component } from '@angular/core';
+import { Component, Injectable, Input, OnInit } from '@angular/core';
+import {
+  FormGroup,
+  FormControl,
+  ReactiveFormsModule,
+  Validators,
+} from '@angular/forms';
+
+class QuestionBase<T> {
+  value: T | undefined;
+  key: string;
+  label: string;
+  required: boolean;
+  order: number;
+  controlType: string;
+  type: string;
+  options: { key: string; value: string }[];
+  constructor(
+    options: {
+      value?: T;
+      key?: string;
+      label?: string;
+      required?: boolean;
+      order?: number;
+      controlType?: string;
+      type?: string;
+      options?: { key: string; value: string }[];
+    } = {}
+  ) {
+    this.value = options.value;
+    this.key = options.key || '';
+    this.label = options.label || '';
+    this.required = !!options.required;
+    this.order = options.order === undefined ? 1 : options.order;
+    this.controlType = options.controlType || '';
+    this.type = options.type || '';
+    this.options = options.options || [];
+  }
+}
+
+@Injectable()
+class QuestionControlService {
+  toFormGroup(questions: QuestionBase<string>[]) {
+    const group: any = {};
+    questions.forEach((question) => {
+      group[question.key] = question.required
+        ? new FormControl(question.value || '', Validators.required)
+        : new FormControl(question.value || '');
+    });
+    return new FormGroup(group);
+  }
+}
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
 })
 export class AppComponent {
-  value = 'World';
+  @Input() questions: QuestionBase<string>[] | null = [];
+  form!: FormGroup;
+  payLoad = '';
+
+  constructor(private qcs: QuestionControlService) {}
+
+  ngOnInit() {
+    this.form = this.qcs.toFormGroup(this.questions as QuestionBase<string>[]);
+  }
 
   onSubmit() {
-    console.log('Submit!');
+    this.payLoad = JSON.stringify(this.form.getRawValue());
   }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,12 +2,12 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
-import { FormsModule } from '@angular/forms';
+import { ReactiveFormsModule } from '@angular/forms';
 import { SharedFormModule } from 'src/libs/shared/form/shared-form.module';
 
 @NgModule({
   declarations: [AppComponent],
-  imports: [BrowserModule, FormsModule, SharedFormModule],
+  imports: [BrowserModule, ReactiveFormsModule, SharedFormModule],
   bootstrap: [AppComponent],
 })
 export class AppModule {}
